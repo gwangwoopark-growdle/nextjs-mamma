111368329973d32ead63d9e6fa3b39d7cc62264a
gwangwoopark <gwangwoo.park@gmail.com>
2022-06-10 15:56:25 +0900

    fix prisma client

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,5 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+// Avoid instantiating too many instances of Prisma in development
+// https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices#problem
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+let client = prisma;
 
 export default client;
