346ed73bdd0fed64e9f4c5b2b5b9154e78d60881
gwangwoopark <gwangwoo.park@gmail.com>
2022-06-10 14:28:15 +0900

    fix apollo client url in client.ts

--- libs/client/client.ts
+++ libs/client/client.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+const HOMEPAGE_URL = process.env.NEXT_PUBLIC_HOMEPAGE_URL;
 
 const client = new ApolloClient({
-  uri: "/api/graphql",
+  uri: HOMEPAGE_URL + "/api/graphql",
   cache: new InMemoryCache(),
 });
 
